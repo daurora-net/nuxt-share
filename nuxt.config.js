ff8748cd1f38234f4fe22c829bc88472eeaa12c3
daurora-net <daurora.net@gmail.com>
2023-08-30 20:27:34 +0900

    apiエンドポイント再度修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,7 +41,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: "https://daudev.com/api",
+    baseURL: "https://daudev.com/api/share",
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
